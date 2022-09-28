ced3f9fbcefabd9a5344a1251525f8ef02b5d0d8
tihomirovsk <pesokote@gmail.com>
2022-09-28 22:23:25 +0300

    DZ done

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
